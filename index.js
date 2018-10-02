11ab620fa41bc5e9b2fd7a139fd571405fc34f3b
Matthew Sherman <msherman0801@gmail.com>
2018-10-02 20:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ for(var i = 0; i<a.length; i++) {
 function deepestChild() {
   var a = document.querySelectorAll('grand-node');
   for (var i = 0; i < a.length; i++) {
-    if (a[i] + 1 === undefined)
+    if (a[i + 1] === undefined)
     return a[i];
   }
 }
